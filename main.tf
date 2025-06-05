fe3009056c1ff33b0a34c769070df4e2dd18869b
alexwebdev05 <alexwebdev05@proton.me>
2025-06-05 12:50:49 +0200

    reverse update

--- main.tf
+++ main.tf
@@ -2,27 +2,3 @@
 provider "kubernetes" {
   config_path    = "~/.kube/config"
 }
-
-# Website
-## Despliege del servicio
-module "website" {
-  source = "./website"
-}
-
-## Actualizedor del repositorio
-resource "kubernetes_manifest" "trigger" {
-  manifest = yamldecode(file("${path.module}/website/trigger.yaml"))
-}
-
-resource "kubernetes_manifest" "gitrepo" {
-  manifest = yamldecode(file("${path.module}/website/gitrepo.yaml"))
-}
-
-resource "kubernetes_manifest" "action" {
-  manifest = yamldecode(templatefile(
-    "${path.module}/website/gitEventRunner/action.yaml.tmpl",
-    {
-      repo_url = "https://github.com/FTEmulator/website.git"
-    }
-  ))
-}
